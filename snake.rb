275ff56c39a64f996a282fc7aed4b6d4a71e22ac
Ravinder Deol <ravinder.r.deol@gmail.com>
2020-03-23 23:29:22 -0400

    snake movement working

--- snake.rb
+++ snake.rb
@@ -2,11 +2,14 @@ require 'ruby2d'
 
 set background: 'navy'
 
+set fps_cap: 15
+
 GRID_SIZE = 20
 
 class Snake
   def initialize
     @positions = [[2, 0], [2, 1], [2, 2], [2, 3]]
+    @direction = 'down'
   end
 
   def draw
@@ -14,9 +17,30 @@ class Snake
       Square.new(x: position[0] * GRID_SIZE, y: position[1] * GRID_SIZE, size: GRID_SIZE - 1, color: 'white')
     end
   end
+
+  def move
+    @positions.shift
+    case @direction
+    when 'down'
+      @positions.push([head[0], head[1] + 1])
+    end
+  end
+
+  private
+
+  def head
+    @positions.last
+  end
 end
 
 snake = Snake.new
 snake.draw
+
+update do
+  clear
+  snake.move
+  snake.draw
+end
+
 
 show
